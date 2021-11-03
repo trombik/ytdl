1381ccfc894cba7c7c7072373849b39f6645ed8d
Tomoyuki Sakurai <y@trombik.org>
2021-11-03 09:11:50 +0700

    implement YTDL::ConfigLoader

---
+++ lib/YTDL/config_loader.rb
@@ -0,0 +1,144 @@
+# frozen_string_literal: true
+
+require "yaml"
+require "erb"
+require "pathname"
+
+class YTDL
+  # A class to load, parse, and validate config file.
+  #
+  # The file is YAML with erb template. You may use valid erb syntax in the
+  # file. Values must conform defined types. Values must be either String or
+  # Integer.
+  #
+  # Use unquoted number in the value for `type` Integer. `"5000"` is String.
+  #
+  # The following example is invalid because `port` is expected to be Integer.
+  #
+  # ```yaml
+  # download_dir: /tmp
+  # port: "5000"
+  # ```
+  #
+  # The following example is valid.
+  #
+  # ```yaml
+  # download_dir: /tmp
+  # port: 5000
+  # ```
+  #
+  # With environment variables, you may override option kyes in the
+  # configuration file and default values. The name of environment variable
+  # must be the key name with a prefix `YTDL_`, and in all upper case.
+  #
+  # Example to override `redis_port`:
+  #
+  # /usr/bin/env YTDL_REDIS_PORT=12345
+  class ConfigLoader
+    VALID_OPTION = {
+      "address" => { type: String, default: "127.0.0.1", required: false },
+      "port" => { type: Integer, default: 5000, required: false },
+      "redis_address" => { type: String, default: "127.0.0.1", required: false },
+      "redis_port" => { type: Integer, default: 6379, required: false },
+      "download_dir" => { type: String, required: true }
+    }.freeze
+
+    def initialize; end
+
+    def required_keys
+      VALID_OPTION.select { |_k, v| v[:required] }.keys
+    end
+
+    def validate_missing_key(config)
+      required_keys.each do |k|
+        raise ArgumentError, format("Key `%<key>s` is required, but missing", key: k) unless config.key? k
+      end
+      config
+    end
+
+    def validate_unknown_key(config)
+      config.each_key do |k|
+        raise ArgumentError, format("Unknown option: `%<key>s`", key: k) unless VALID_OPTION.keys.include? k
+      end
+      config
+    end
+
+    def validate_type(config)
+      config.each do |k, v|
+        unless v.is_a? VALID_OPTION[k][:type]
+          raise ArgumentError,
+                format("Key `%<key>s` must be %<type>s, but `%<actual>s`", key: k, type: VALID_OPTION[k], actual: v)
+        end
+      end
+      config
+    end
+
+    def validate(config)
+      validate_missing_key(config)
+      validate_unknown_key(config)
+      validate_type(config)
+      config
+    end
+
+    def read_file(path)
+      File.read(path)
+    rescue StandardError => e
+      warn format("Cannot read configuration file `%<path>s`", path: Pathname.pwd + path)
+      raise e
+    end
+
+    def read_erb(config)
+      ERB.new(config).result
+    end
+
+    def read_yaml(arg)
+      result = YAML.safe_load(arg)
+      # XXX when input is empty, safe_load returns false
+      raise ArgumentError, "empty YAML" unless result
+
+      result
+    end
+
+    def default_option
+      default = {}
+      VALID_OPTION.each do |k, v|
+        raise format("BUG: Key `%<key>s` is required but has `:default`", key: k) if v[:required] && v[:default]
+
+        if !v[:required] && !v.key?(:default)
+          raise format("BUG: Key `%<key>s` is not required but has no `:default`",
+                       key: k)
+        end
+
+        default[k] = v[:default] if v.key?(:default)
+      end
+      default
+    end
+
+    def merge_default(config)
+      default_option.merge(config)
+    end
+
+    def override_with_env(config)
+      config.each_key do |k|
+        env_key = "YTDL_#{k.upcase}"
+        next unless ENV.key?(env_key)
+
+        unless ENV[env_key].match(/^\d+$/)
+          raise ArgumentError, format("`%<key>s` in ENV is not Integer: `%<value>s`", key: env_key, value: ENV[env_key])
+        end
+
+        config[k] = VALID_OPTION[k][:type] == Integer ? ENV[env_key].to_i : ENV[env_key]
+      end
+      config
+    end
+
+    def load_file(path)
+      config = read_file(path)
+      config = read_erb(config)
+      config = read_yaml(config)
+      config = merge_default(config)
+      config = override_with_env(config)
+      validate(config)
+    end
+  end
+end
---
+++ spec/lib/YTDL/config_loader_spec.rb
@@ -0,0 +1,178 @@
+# frozen_string_literal: true
+
+require_relative "../../spec_helper"
+require_relative "../../../lib/YTDL/config_loader"
+
+describe YTDL::ConfigLoader do
+  let(:valid_erb) do
+    <<-YAML
+    address: 127.0.0.1
+    port: <%= 4999 + 1 %>
+    redis_address: 127.0.0.1
+    redis_port: 6379
+    download_dir: /tmp
+    YAML
+  end
+
+  let(:invalid_erb) do
+    <<-YAML
+    foo: bar
+    buz: yes
+    YAML
+  end
+
+  let(:obj) { YTDL::ConfigLoader.new }
+
+  describe "#new" do
+    it "creates object" do
+      expect { YTDL::ConfigLoader.new }.not_to raise_error
+    end
+  end
+
+  describe "#read_erb" do
+    it "parse arg as erb" do
+      allow(obj).to receive(:read_file).and_return(valid_erb)
+      expect { obj.read_erb("/foo/bar") }.not_to raise_error
+    end
+  end
+
+  describe "#read_yaml" do
+    it "parse arg as YAML" do
+      result = ""
+      expect do
+        result = obj.read_erb(valid_erb)
+        obj.read_yaml(result)
+      end.not_to raise_error
+    end
+
+    context "When content is empty" do
+      it "does not raise" do
+        expect { obj.read_yaml("") }.to raise_error ArgumentError
+      end
+    end
+  end
+
+  describe "#required_keys" do
+    it "includes download_dir" do
+      expect(obj.required_keys).to include("download_dir")
+    end
+  end
+  describe "#validate_missing_key" do
+    context "When required key is missing" do
+      it "raises ArgumentError" do
+        expect { obj.validate_missing_key({ foo: "bar" }) }.to raise_error ArgumentError
+      end
+    end
+  end
+
+  describe "#validate_unknown_key" do
+    context "When unknown key is found" do
+      it "raises ArgumentError" do
+        expect { obj.validate_unknown_key({ foo: "bar" }) }.to raise_error ArgumentError
+      end
+    end
+  end
+
+  describe "#validate_type" do
+    context "When Integer is expected but Srting is given" do
+      it "raises ArgumentError" do
+        expect { obj.validate_type({ "port" => "5000" }) }.to raise_error ArgumentError
+      end
+    end
+  end
+
+  describe "#validate" do
+    context "when valid config is given" do
+      it "does not raise" do
+        result = ""
+        expect do
+          obj.validate({"download_dir" => "/tmp"})
+        end.not_to raise_error
+      end
+    end
+    context "when invalid config is given" do
+      it "raises" do
+        result = ""
+        expect do
+          result = obj.read_erb(invalid_erb)
+          result = obj.read_yaml(result)
+          result = obj.validate(result)
+        end.to raise_error ArgumentError
+      end
+    end
+  end
+
+  describe "#merge_default" do
+    context "When optional key is missing" do
+      it "merge DEFAULT_OPTION" do
+        result = obj.merge_default({ "address" => "foo.example.org" })
+        expect(result["address"]).to eq "foo.example.org"
+        expect(result["port"]).to eq 5000
+      end
+    end
+  end
+
+  describe "#override_with_env" do
+    context "when same key with YTDL_ prefix is set in environment" do
+      before(:each) { ENV["YTDL_REDIS_PORT"] = "16379" }
+      after(:each) { ENV.delete("YTDL_REDIS_PORT") }
+
+      it "overrides the key in the config" do
+        result = obj.override_with_env({ "redis_port" => 6379 })
+        expect(result["redis_port"]).to eq 16_379
+      end
+    end
+  end
+
+  describe "#load_file" do
+    it "loads file and returns config" do
+      allow(obj).to receive(:read_file).and_return(valid_erb)
+      result = obj.load_file("/foo/bar")
+      expect(result).to include("address" => "127.0.0.1")
+      expect(result).to include("port" => 5000)
+      expect(result).to include("redis_address" => "127.0.0.1")
+      expect(result).to include("redis_port" => 6379)
+      expect(result).to include("download_dir" => "/tmp")
+    end
+
+    context "When invalid key is given" do
+      it "raises ArgumentError" do
+        content = <<-CONFIG
+        download_dir: /tmp
+        foo: bar
+        CONFIG
+        allow(obj).to receive(:read_file).and_return(content)
+        expect { obj.load_file("/foo/bar") }.to raise_error ArgumentError
+      end
+    end
+
+    context "When required key is missing" do
+      it "raises ArgumentError" do
+        expect(obj).to receive(:read_file).and_return("port: 5000")
+        expect { obj.load_file("/foo/bar") }.to raise_error ArgumentError
+      end
+    end
+
+    context "When redis_port is overriden by YTDL_REDIS_PORT" do
+      before(:each) { ENV["YTDL_REDIS_PORT"] = "16379" }
+      after(:each) { ENV.delete("YTDL_REDIS_PORT") }
+
+      it "returns config with YTDL_REDIS_PORT" do
+        expect(obj).to receive(:read_file).and_return("download_dir: /tmp")
+        result = obj.load_file("/foo/bar")
+        expect(result["redis_port"]).to be 16_379
+      end
+    end
+
+    context "When redis_port is overriden by YTDL_REDIS_PORT but the value is String" do
+      before(:each) { ENV["YTDL_REDIS_PORT"] = "foo" }
+      after(:each) { ENV.delete("YTDL_REDIS_PORT") }
+
+      it "raises ArgumentError" do
+        allow(obj).to receive(:read_file).and_return("download_dir: /tmp")
+
+        expect { obj.load_file("/foo/bar") }.to raise_error ArgumentError
+      end
+    end
+  end
+end
